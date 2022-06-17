4ed3296f9212b30a612a496b99709e5926b6f6f8
Pratham Verma <prathamverma12345@gmail.com>
2022-06-17 12:09:02 +0530

    added

---
+++ LinkedList/2. MiddleOfAList.cpp
@@ -0,0 +1,10 @@
+// Slow-faster Point approach
+ListNode* middleNode(ListNode* head) {
+    ListNode*slow = head;
+    ListNode*fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
